186092fecde080d184b4799ef9e57ec743c8b1d7
Nazwa Aulia <116379613+Waarchaa@users.noreply.github.com>
2022-12-08 20:12:55 +0700

    Add files via upload

---
+++ Segitiga.java
@@ -0,0 +1,18 @@
+public class Segitiga extends BangunDatar {
+    int alas, tinggi;
+  
+    public Segitiga(int alas, int tinggi) {
+      this.alas = alas;
+      this.tinggi = tinggi;
+    }
+  
+    @Override
+    public float luas() {
+      return (float) (0.5 * (this.alas * this.tinggi));
+    }
+  
+    @Override
+    public void gambar() {
+      System.out.println("Drawing Triangle");
+    }
+  }
